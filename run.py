c9178c6cf42b0938ae3f1a51f0d0627a31e777c8
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-04-15 18:43:40 +0200

    New format

--- run.py
+++ run.py
@@ -1,16 +1,13 @@
 import numpy as np
-from visualize import Visualize
-from example_data import example_algorithm, load_data
+import polars as pl
 
 if __name__ == "__main__":
-    data, target = load_data("w_data")
-    print("Data shape: ", data.shape)
-    print("Target shape: ", target.shape)
-    print("Type data: ", type(data))
-    print("Type target: ", type(target))
-    print("Data[0] types: ", type(data[0][0]), type(data[0][1]))
-    print("Target[0] type: ", type(target[0]))
-    
-    visualizer = Visualize(data=data, target=target, title="W data", labels=['a', 'b'])
-    visualizer.visualize(type='2D')
+    data = pl.read_csv('data1.csv', separator=',')
+    desc_data = pl.read_csv('DISCdata1.csv', separator=',')
+
+    print(data)
+    print(type(data))
+    print(desc_data)
+    print(type(desc_data))
+
     
