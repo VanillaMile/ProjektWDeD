64f88019394bffa6e46525299c114ba886d01aed
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-04-15 19:36:55 +0200

    Tests class

---
+++ Tests.py
@@ -0,0 +1,8 @@
+import polars as pl
+import numpy as np
+
+class Tests:    
+    def check_data(self, data_path: str) -> None:
+        pass
+
+    # Other methods here
--- run.py
+++ run.py
@@ -1,13 +1,33 @@
 import numpy as np
 import polars as pl
 
+from Tests import Tests
+
+def example_algorithm(data_path: str) -> None:
+    pass
+
 if __name__ == "__main__":
-    data = pl.read_csv('data1.csv', separator=',')
-    desc_data = pl.read_csv('DISCdata1.csv', separator=',')
+    # Implement method to input these paths
+    data_paths = ['data1.csv']
+    disc_data_paths = ['DISCdata1.csv']
+
+    data = pl.read_csv(data_paths[0], separator=',')
+    disc_data = pl.read_csv(disc_data_paths[0], separator=',')
 
     print(data)
     print(type(data))
-    print(desc_data)
-    print(type(desc_data))
+    print(disc_data)
+    print(type(disc_data))
+
+    tests = Tests()
+
+    # Measure time of this
+    for path in data_paths:
+        example_algorithm(path)
+    
+    for path in disc_data_paths:
+        tests.check_data(path)
+        
+         
 
     
