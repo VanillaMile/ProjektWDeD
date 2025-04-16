8900b572ede6f17412ee134b091063708524ed63
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-04-16 15:01:26 +0200

    Better data load and bad column names fix
    Fixed column names, previously first row of csv was treated as column names...

--- Tests.py
+++ Tests.py
@@ -1,8 +1,23 @@
 import polars as pl
 import numpy as np
 
-class Tests:    
-    def check_data(self, data_path: str) -> None:
-        pass
+class Tests:
+    def __init__(self, data_path: str, disc_data_path: str) -> None:
+        self.data = pl.read_csv(data_path, separator=',', has_header=False)
+        self.disc_data = pl.read_csv(disc_data_path, separator=',', has_header=False)
+
+        labels = ['x' + str(i) for i in range(self.data.shape[1] - 1)]
+        labels.append('Dec')
+
+        self.data.columns = labels
+        self.disc_data.columns = labels
+        
+
+    def check_data(self) -> None:
+        print(self.data)
+        print(self.disc_data)
+
+        print(self.data['Dec'])
+
 
     # Other methods here
--- run.py
+++ run.py
@@ -11,23 +11,10 @@ if __name__ == "__main__":
     data_paths = ['data1.csv']
     disc_data_paths = ['DISCdata1.csv']
 
-    data = pl.read_csv(data_paths[0], separator=',')
-    disc_data = pl.read_csv(disc_data_paths[0], separator=',')
-
-    print(data)
-    print(type(data))
-    print(disc_data)
-    print(type(disc_data))
-
-    tests = Tests()
-
     # Measure time of this
     for path in data_paths:
         example_algorithm(path)
     
-    for path in disc_data_paths:
-        tests.check_data(path)
-        
-         
-
-    
+    for data_path, disc_data_path in zip(data_paths, disc_data_paths):
+        tests = Tests(data_path, disc_data_path)
+        tests.check_data()
