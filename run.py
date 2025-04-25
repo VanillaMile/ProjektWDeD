03c6f6b7eaa593be2c0bc11af413d8ce84edc303
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-04-25 14:59:49 +0200

    Cleaning run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import numpy as np
 import polars as pl
-from example_data import iris2D, iris3D, iris3DBAD, NoDecisionRange
 from Tests import Tests
 
 def example_algorithm(data_path: str) -> None:
@@ -8,8 +7,8 @@ def example_algorithm(data_path: str) -> None:
 
 if __name__ == "__main__":
     # Implement method to input these paths
-    data_paths = ['data1.csv', 'iris2D.csv', 'iris3D.csv', 'iris3D.csv', 'nodec.csv']
-    disc_data_paths = ['DISCdata1.csv', 'DISCiris2D.csv', 'DISCiris3D.csv', 'DISCiris3DBAD.csv', 'DISCnodec.csv']
+    data_paths = ['data1.csv']
+    disc_data_paths = ['DISCdata1.csv']
 
     # Measure time of this
     for path in data_paths:
@@ -18,12 +17,3 @@ if __name__ == "__main__":
     for data_path, disc_data_path in zip(data_paths, disc_data_paths):
         tests = Tests(data_path, disc_data_path, has_header=False)
         tests.check_data()
-
-    # iris = iris2D()
-    # iris.plot()
-    iris = iris3D()
-    iris.plot()
-    # iris = iris3DBAD()
-    # iris.plot()
-    # nodec = NoDecisionRange()
-    # nodec.plot()
